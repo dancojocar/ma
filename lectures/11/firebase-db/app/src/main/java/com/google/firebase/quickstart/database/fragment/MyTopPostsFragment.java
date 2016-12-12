108116e9300b131dbbc85943410bf1120659af8a
Dan Cojocar <dan@cs.ubbcluj.ro>
2016-12-12 22:14:47 +0200

    Lecture #11

---
+++ lectures/11/firebase-db/app/src/main/java/com/google/firebase/quickstart/database/fragment/MyTopPostsFragment.java
@@ -0,0 +1,19 @@
+package com.google.firebase.quickstart.database.fragment;
+
+import com.google.firebase.database.DatabaseReference;
+import com.google.firebase.database.Query;
+
+public class MyTopPostsFragment extends PostListFragment {
+
+    public MyTopPostsFragment() {}
+
+    @Override
+    public Query getQuery(DatabaseReference databaseReference) {
+        // My top posts by number of stars
+        String myUserId = getUid();
+        Query myTopPostsQuery = databaseReference.child("user-posts").child(myUserId)
+                .orderByChild("starCount");
+
+        return myTopPostsQuery;
+    }
+}
