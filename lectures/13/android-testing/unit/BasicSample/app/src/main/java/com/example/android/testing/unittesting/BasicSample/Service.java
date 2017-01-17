c1072218801e440850dbf04632a72dc8ed25d435
Dan Cojocar <dan@cs.ubbcluj.ro>
2017-01-17 12:56:34 +0200

    More sample tests

---
+++ lectures/13/android-testing/unit/BasicSample/app/src/main/java/com/example/android/testing/unittesting/BasicSample/Service.java
@@ -0,0 +1,9 @@
+package com.example.android.testing.unittesting.BasicSample;
+
+/**
+ * Created by dan on 1/10/17.
+ */
+
+public interface Service {
+    boolean login(String user, String pass);
+}
---
+++ lectures/13/android-testing/unit/BasicSample/app/src/test/java/com/example/android/testing/unittesting/BasicSample/ServiceTest.java
@@ -0,0 +1,46 @@
+package com.example.android.testing.unittesting.BasicSample;
+
+import android.test.suitebuilder.annotation.SmallTest;
+
+import org.junit.Before;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mock;
+import org.mockito.runners.MockitoJUnitRunner;
+
+import static org.hamcrest.core.Is.is;
+import static org.hamcrest.core.IsEqual.equalTo;
+import static org.junit.Assert.assertThat;
+import static org.mockito.Matchers.anyString;
+import static org.mockito.Matchers.eq;
+import static org.mockito.Mockito.when;
+
+/**
+ * Created by dan on 1/10/17.
+ */
+
+@RunWith(MockitoJUnitRunner.class)
+@SmallTest
+public class ServiceTest {
+
+    @Mock
+    private Service service;
+
+    @Before
+    public void setup(){
+//        service = new RealServiceImpl();
+        when(service.login(eq("test@test.ro"),anyString())).thenReturn(true);
+        when(service.login(eq("test@test.com"),anyString())).thenReturn(false);
+    }
+
+    @Test
+    public void testLogin(){
+        boolean login = service.login("test@test.ro", "1234");
+        assertThat("testing proper login",login,is(equalTo(true)));
+    }
+    @Test
+    public void testFaildLogin(){
+        boolean login = service.login("test@test.com", "1234");
+        assertThat("testing failed login",login,is(equalTo(false)));
+    }
+}
