1f0b45469cdc99d262c727d386a8301eb870bb81
Dan Cojocar <dan.cojocar@gmail.com>
2017-10-15 18:29:44 +0300

    Hello world android app

---
+++ labs/1/MyApplication/app/src/main/java/com/example/dan/androidapp/MainActivity.java
@@ -0,0 +1,16 @@
+package com.example.dan.androidapp;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+import android.util.Log;
+
+public class MainActivity extends AppCompatActivity {
+    private static final String TAG = MainActivity.class.getName();
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+        Log.d(TAG, "onCreate");
+    }
+}
