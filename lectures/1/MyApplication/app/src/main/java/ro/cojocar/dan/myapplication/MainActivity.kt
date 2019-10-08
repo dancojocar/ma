f2c8cf5709328d31aad320ad576606e9260f53a6
Dan Cojocar <dan.cojocar@gmail.com>
2019-10-08 20:26:03 +0300

    Lecture #1

---
+++ lectures/1/MyApplication/app/src/main/java/ro/cojocar/dan/myapplication/MainActivity.kt
@@ -0,0 +1,43 @@
+package ro.cojocar.dan.myapplication
+
+import android.os.Bundle
+import android.support.v7.app.AppCompatActivity
+
+class MainActivity : AppCompatActivity() {
+
+    override fun onCreate(savedInstanceState: Bundle?) {
+        super.onCreate(savedInstanceState)
+        setContentView(R.layout.activity_main)
+        logd("onCreate called")
+    }
+
+    override fun onStart() {
+        super.onStart()
+        logd("onStart called")
+    }
+
+    override fun onResume() {
+        super.onResume()
+        logd("onResume called")
+    }
+
+    override fun onPause() {
+        super.onPause()
+        logd("onPause called")
+    }
+
+    override fun onStop() {
+        super.onStop()
+        logd("onStop called")
+    }
+
+    override fun onRestart() {
+        super.onRestart()
+        logd("onRestart called")
+    }
+
+    override fun onDestroy() {
+        super.onDestroy()
+        logd("onDestroy called")
+    }
+}
