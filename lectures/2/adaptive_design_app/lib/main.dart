66b8cc02b2868a49570155b574c70e1cda464f7f
Dan Cojocar <dan.cojocar@gmail.com>
2019-10-09 11:48:20 +0300

    Lecture #1

---
+++ lectures/2/adaptive_design_app/lib/main.dart
@@ -0,0 +1,18 @@
+import 'package:flutter/material.dart';
+import 'package:adaptive_design_app/MasterDetailPage.dart';
+
+void main() => runApp(MyApp());
+
+class MyApp extends StatelessWidget {
+  @override
+  Widget build(BuildContext context) {
+    return MaterialApp(
+      title: 'Adaptive List Demo',
+      theme: ThemeData(
+        primarySwatch: Colors.blue,
+      ),
+      home: MasterDetailPage(),
+      debugShowCheckedModeBanner: false,
+    );
+  }
+}
