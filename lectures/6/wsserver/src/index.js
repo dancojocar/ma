fec790723473f5745789b9db95ed6a8656e4a079
Dan Cojocar <dan.cojocar@gmail.com>
2019-11-06 16:51:31 +0200

    Lecture #6 Broadcast server.

--- lectures/6/wsserver/src/index.js
+++ lectures/6/wsserver/src/index.js
@@ -11,16 +11,26 @@ app.ws.use(function (ctx, next) {
   return next(ctx);
 });
 
+let clients = [];
+
+const broadcast = (data) => {
+  clients.forEach((client) => {
+      client.send(data);
+  });
+};
+
 // Using routes
 app.ws.use(route.all('/', function (ctx) {
   // `ctx` is the regular koa context created from the `ws` onConnection `socket.upgradeReq` object.
   // the websocket is added to the context on `ctx.websocket`.
   ctx.websocket.send('Hello from the other side!');
+  clients.push(ctx.websocket);
   ctx.websocket.on('message', function (message) {
     // do something with the message from client
     console.log(message);
-    if (message)
-      ctx.websocket.send('Echo: ' + message);
+    if (message) {
+      broadcast(message)
+    }
   });
 }));
 
