6b584d7b3e461e3e78a38e1e1927cc4933dc67b9
Dan Cojocar <dan.cojocar@gmail.com>
2020-11-09 07:13:50 +0200

    Replace content for lecture #6

--- lectures/6/android-ContentProviderPaging/app/src/main/java/com/example/android/contentproviderpaging/Utils.kt
+++
@@ -1,7 +0,0 @@
-package com.example.android.contentproviderpaging
-
-import android.util.Log
-
-fun Any.logd(message: String = "No message", cause: Throwable? = null) {
-  Log.d(this.javaClass.simpleName, message, cause)
-}
