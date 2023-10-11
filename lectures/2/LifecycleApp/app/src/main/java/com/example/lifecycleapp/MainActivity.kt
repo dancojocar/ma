8a6073f0fd99ae48885520bf980ca96994116cd2
Dan Cojocar <dan.cojocar@gmail.com>
2023-10-11 08:45:40 +0300

    Update LifecycleApp

--- lectures/2/LifecycleApp/app/src/main/java/com/example/lifecycleapp/MainActivity.kt
+++ lectures/2/LifecycleApp/app/src/main/java/com/example/lifecycleapp/MainActivity.kt
@@ -1,13 +1,12 @@
 package com.example.lifecycleapp
 
 import android.os.Bundle
-import android.util.Log
 import androidx.activity.ComponentActivity
 import androidx.activity.compose.setContent
 import androidx.compose.foundation.layout.fillMaxSize
-import androidx.compose.material.MaterialTheme
-import androidx.compose.material.Surface
-import androidx.compose.material.Text
+import androidx.compose.material3.MaterialTheme
+import androidx.compose.material3.Surface
+import androidx.compose.material3.Text
 import androidx.compose.runtime.Composable
 import androidx.compose.ui.Modifier
 import androidx.compose.ui.tooling.preview.Preview
@@ -21,7 +20,7 @@ class MainActivity : ComponentActivity() {
         // A surface container using the 'background' color from the theme
         Surface(
           modifier = Modifier.fillMaxSize(),
-          color = MaterialTheme.colors.background
+          color = MaterialTheme.colorScheme.background
         ) {
           Greeting("Android")
         }
@@ -51,8 +50,11 @@ class MainActivity : ComponentActivity() {
 }
 
 @Composable
-fun Greeting(name: String) {
-  Text(text = "Hello $name!")
+fun Greeting(name: String, modifier: Modifier = Modifier) {
+  Text(
+    text = "Hello $name!",
+    modifier = modifier
+  )
 }
 
 @Preview(showBackground = true)
