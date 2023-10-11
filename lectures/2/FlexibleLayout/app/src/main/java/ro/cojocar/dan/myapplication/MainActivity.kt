df3df1c36fa61c66fe2b8c72a4014c498f5a634b
Dan Cojocar <dan.cojocar@gmail.com>
2023-10-11 08:46:07 +0300

    Update old flexible layout demo

--- lectures/2/FlexibleLayout/app/src/main/java/ro/cojocar/dan/myapplication/MainActivity.kt
+++ lectures/2/FlexibleLayout/app/src/main/java/ro/cojocar/dan/myapplication/MainActivity.kt
@@ -1,18 +1,28 @@
 package ro.cojocar.dan.myapplication
 
 import android.os.Bundle
+import android.widget.TextView
 import androidx.appcompat.app.AppCompatActivity
 import kotlinx.android.synthetic.main.activity_main.*
+import ro.cojocar.dan.myapplication.databinding.ActivityMainBinding
 
 class MainActivity : AppCompatActivity() {
+  private lateinit var binding: ActivityMainBinding
 
-    override fun onCreate(savedInstanceState: Bundle?) {
-        super.onCreate(savedInstanceState)
-        setContentView(R.layout.activity_main)
-        button.setOnClickListener {
-            text.text = getString(R.string.fromText, editText.text)
-            button.text = getString(R.string.defaultText)
-        }
-        logd("onCreate called")
+  override fun onCreate(savedInstanceState: Bundle?) {
+    super.onCreate(savedInstanceState)
+    binding = ActivityMainBinding.inflate(layoutInflater)
+    val view = binding.root
+    setContentView(view)
+    button.setOnClickListener {
+      val message = getString(R.string.fromText, editText.text)
+      //initial api
+      findViewById<TextView>(R.id.text).text = message
+      //using synthetic import
+      button.text = getString(R.string.defaultText)
+      //using new recommmended view binding way
+      binding.text2.text = message
     }
+    logd("onCreate called")
+  }
 }
